ab61ad3c3a00ac597790d700564f324ce18ea7f7
luc_jaubert <j.luc@obvy-app.com>
2024-05-06 14:42:25 +0200

    wip transtion component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,14 +2,22 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http'; 
 import { FooterComponent } from './shared/components/footer/footer.component';
+import { HeaderComponent } from './shared/components/header/header.component';
+import { TransitionComponent } from './shared/components/transition/transition.component';
 
 @Component({
     selector: 'app-root',
     standalone: true,
-    imports: [RouterOutlet, HttpClientModule, FooterComponent], 
+    imports: [RouterOutlet, HttpClientModule, FooterComponent, HeaderComponent, TransitionComponent], 
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
     title = 'laiterie-burdigala';
+    showTransition: boolean = false;
+    isHomepage: boolean = false
+
+    toggleTransition(): void {
+        this.showTransition = !this.showTransition;
+    }
 }
