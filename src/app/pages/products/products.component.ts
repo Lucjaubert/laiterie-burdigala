eb04d3468faac0fb29133c3679688b21a6ad668e
luc_jaubert <j.luc@obvy-app.com>
2024-05-17 14:42:06 +0200

    section products wip

--- src/app/pages/products/products.component.ts
+++ src/app/pages/products/products.component.ts
@@ -1,7 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { Observable, of } from 'rxjs';
-import { catchError } from 'rxjs/operators';
+import { catchError, map } from 'rxjs/operators';
 import { WordpressService } from 'src/app/services/wordpress.service';
 import { HeaderComponent } from '../../shared/components/header/header.component';
 import { FormsModule } from '@angular/forms';
@@ -17,13 +17,9 @@ interface ProductData {
     product_category: string;
   }
 }
-
-interface PageContent {
-  acf_fields: {
-    title: string;
-    title_2: string;
-    subtitle: string;
-  }
+interface CategoryGroup {
+  category: string;
+  products: ProductData[];
 }
 
 @Component({
@@ -35,27 +31,38 @@ interface PageContent {
 })
 export class ProductsComponent implements OnInit {
   isHomepage = false;
-
   productsData$: Observable<ProductData[] | null>;
-  pageContent$: Observable<PageContent | null>;
+  productsByCategory$: Observable<CategoryGroup[]>;
 
   constructor(private wpService: WordpressService) {
-    this.pageContent$ = this.wpService.getPageContent().pipe(
-      catchError(error => {
-        console.error('Erreur lors de la récupération des données de la page:', error);
-        return of(null);
-      })
-    );
-
     this.productsData$ = this.wpService.getProducts().pipe(
       catchError(error => {
         console.error('Erreur lors de la récupération des données des produits:', error);
-        return of(null);
+        return of(null); // Fallback to null in case of error
       })
+    );
+
+    this.productsByCategory$ = this.productsData$.pipe(
+      map(products => this.groupProductsByCategory(products))
     );
   }
 
   ngOnInit(): void {}
+
+  private groupProductsByCategory(products: ProductData[] | null): CategoryGroup[] {
+    const categoryGroups: { [key: string]: ProductData[] } = {};
+    products?.forEach(product => {
+      const category = product.acf_fields.product_category || 'Other';
+      if (!categoryGroups[category]) {
+        categoryGroups[category] = [];
+      }
+      categoryGroups[category].push(product);
+    });
+    return Object.keys(categoryGroups).map(key => ({
+      category: key,
+      products: categoryGroups[key]
+    }));
+  }
 
   decreaseQuantity(product: ProductData): void {
     if (product.acf_fields.default_quantity > 1) {
@@ -68,7 +75,6 @@ export class ProductsComponent implements OnInit {
   }
 
   addToOrder(product: ProductData): void {
-    // Logique pour ajouter le produit à la commande
     console.log('Ajouter à la commande:', product);
   }
 }
